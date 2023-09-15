12d4fa632708967fe856bd8167bfbfa597ca4e6e
ArianMathai <123459777+ArianMathai@users.noreply.github.com>
2023-09-15 12:19:37 +0200

    refactored server with request and response

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,47 +1,33 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
 public class HttpServer {
 
     public final ServerSocket serverSocket;
-    public final String requestLine;
-    public final Map<String, String> headers;
 
 
     public HttpServer(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
-        headers = new HashMap<>();
 
+        while (true){
         Socket clientSocket = serverSocket.accept();
+        HttpRequest request = new HttpRequest(clientSocket);
+        HttpResponse response = new HttpResponse();
 
-        requestLine = readLine(clientSocket);
-        System.out.println(requestLine);;
 
-        String headerLine;
-        while ( !(headerLine = readLine(clientSocket)).isEmpty()) {
-            String[] headerParts = headerLine.split(" *: *",2);
-            headers.put(headerParts[0], headerParts[1]);
+
+
+        clientSocket.getOutputStream().write(
+                response.getResponse().getBytes(StandardCharsets.UTF_8));
+        clientSocket.getOutputStream().close();
+        clientSocket.close();
+
+
+
         }
-        printThings();
 
     }
-    private void printThings(){
-        System.out.println(requestLine);
-        headers.entrySet().forEach(System.out::println);
-    }
 
-    private String readLine(Socket socket) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int character;
-
-        while( (character = socket.getInputStream().read() ) != '\r'){
-            sb.append( (char) character);
-        }
-        character = socket.getInputStream().read();
-
-        return sb.toString();
-    }
 }
